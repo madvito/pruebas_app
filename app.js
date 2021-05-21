e16b2b70502739f28352ef5ee4ec713fcc3aed06
Victor <vitomad89@gmail.com>
2021-05-21 15:02:03 -0400

    separacion env

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const router = require('./routes');
 require('dotenv').config();
 
+console.log('NODE_ENV',process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development'){
+    require('dotenv').config({path: `${__dirname}/.env.dev`});
+}else if (process.env.NODE_ENV === 'production'){
+    require('dotenv').config();
+}
+
 const app = express();
 app.use(express.json());
 
@@ -15,7 +23,11 @@ app.use(router);
 const SERVER_PORT = process.env.SERVER_PORT;
 
 mongoose.connect('mongodb://localhost:27017/pruebas_app', 
-{useNewUrlParser: true, useUnifiedTopology: true},()=>{
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+},()=>{
     app.listen(SERVER_PORT, ()=>{
         console.log(`Listening http:localhost:${SERVER_PORT}`)
     });
